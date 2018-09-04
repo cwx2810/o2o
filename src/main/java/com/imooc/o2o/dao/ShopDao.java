b993861ceeb62ff4dcd47d5831df07c301512cd5
cwx2810 <cwx2810@163.com>
2018-09-04 11:19:36 +0800

    insert shop dao finished

---
+++ src/main/java/com/imooc/o2o/dao/ShopDao.java
@@ -0,0 +1,16 @@
+package com.imooc.o2o.dao;
+
+import com.imooc.o2o.entity.Shop;
+
+/**
+ * @author: LieutenantChen
+ * @create: 2018-09-03 23:37
+ **/
+public interface ShopDao {
+    /**
+     * 新增店铺
+     * @param shop
+     * @return 1插入成功，-1插入失败
+     */
+    int insertShop(Shop shop);
+}
---
+++ src/test/java/com/imooc/o2o/dao/ShopDaoTest.java
@@ -0,0 +1,52 @@
+package com.imooc.o2o.dao;
+
+import com.imooc.o2o.BaseTest;
+import com.imooc.o2o.entity.Area;
+import com.imooc.o2o.entity.PersonInfo;
+import com.imooc.o2o.entity.Shop;
+import com.imooc.o2o.entity.ShopCategory;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author: LieutenantChen
+ * @create: 2018-09-04 10:00
+ **/
+public class ShopDaoTest extends BaseTest {
+
+    @Autowired
+    private ShopDao shopDao;
+
+    @Test
+    public void testInsertShop() {
+        Shop shop = new Shop();
+
+        // 读取3个对象实例
+        PersonInfo personInfo = new PersonInfo();
+        Area area = new Area();
+        ShopCategory shopCategory = new ShopCategory();
+        personInfo.setUserId(1L);
+        area.setAreaId(2L);
+        shopCategory.setShopCategoryId(1L);
+
+        shop.setOwner(personInfo);
+        shop.setArea(area);
+        shop.setShopCategory(shopCategory);
+        shop.setShopName("测试店铺");
+        shop.setShopDesc("test");
+        shop.setShopAddr("test");
+        shop.setShopImg("test");
+        shop.setPhone("1234567");
+        shop.setPriority(2);
+        shop.setEnableStatus(1);
+        shop.setAdvice("审核中");
+        shop.setCreateTime(new Date());
+
+        int effectedNum = shopDao.insertShop(shop);
+        assertEquals(1, effectedNum);
+    }
+}
