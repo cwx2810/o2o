1606041ee959564aac2f1914873bc0389e98585b
cwx2810 <cwx2810@163.com>
2018-09-08 18:29:17 +0800

    商品分类批量添加前后端联调

--- src/main/java/com/imooc/o2o/web/shopadmin/ProductCategoryManageController.java
+++ src/main/java/com/imooc/o2o/web/shopadmin/ProductCategoryManageController.java
@@ -1,18 +1,23 @@
 package com.imooc.o2o.web.shopadmin;
 
+import com.imooc.o2o.dto.ProductCategoryExecution;
 import com.imooc.o2o.dto.Result;
 import com.imooc.o2o.entity.ProductCategory;
 import com.imooc.o2o.entity.Shop;
 import com.imooc.o2o.enums.ProductCategoryStateEnum;
+import com.imooc.o2o.exceptions.ProductCategoryException;
 import com.imooc.o2o.service.ProductCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: LieutenantChen
@@ -55,6 +60,46 @@ public class ProductCategoryManageController {
                     productCategoryStateEnum.getState(),
                     productCategoryStateEnum.getStateInfo());
         }
+    }
+
+    /**
+     * 批量添加商品类别
+     * @param productCategoryList
+     * @param httpServletRequest
+     * @return
+     */
+    @RequestMapping(value = "/addproductcategorys", method = RequestMethod.POST)
+    @ResponseBody
+    private Map<String, Object> addProductCategorys(
+            @RequestBody List<ProductCategory> productCategoryList,
+                         HttpServletRequest httpServletRequest) {
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        Shop currentShop = (Shop) httpServletRequest.getSession().getAttribute("currentShop");
+        // 给每个商品分类赋予从session中取出的shopId
+        for (ProductCategory productCategory : productCategoryList) {
+            productCategory.setShopId(currentShop.getShopId());
+        }
+        // 如果有商品分类，则添加进去
+        if (productCategoryList != null && productCategoryList.size() > 0) {
+            try {
+                ProductCategoryExecution productCategoryExecution = productCategoryService.batchAddProductCategory(productCategoryList);
+                // 添加进去后得到的结果是成功的好说，如果不成功，返回错误原因
+                if (productCategoryExecution.getState() == ProductCategoryStateEnum.SUCCESS.getState()) {
+                    modelMap.put("success", true);
+                } else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", productCategoryExecution.getStateInfo());
+                }
+            } catch (Exception e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.getMessage());// 捕获异常
+                return modelMap;
+            }
+        } else {
+            modelMap.put("succes", false);
+            modelMap.put("errMsg", "请添加一个商品分类");
+        }
+        return modelMap;
     }
 
 
