9b83120baef5b0f58161ecf5565be7352e42f75f
cwx2810 <cwx2810@163.com>
2018-09-12 15:48:01 +0800

    头条查询dao

---
+++ src/test/java/com/imooc/o2o/dao/HeadLineDaoTest.java
@@ -0,0 +1,26 @@
+package com.imooc.o2o.dao;
+
+import com.imooc.o2o.BaseTest;
+import com.imooc.o2o.entity.HeadLine;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author: LieutenantChen
+ * @create: 2018-09-12 14:43
+ **/
+public class HeadLineDaoTest extends BaseTest {
+
+    @Autowired
+    private HeadLineDao headLineDao;
+
+    @Test
+    public void testQueryHeadLine() {
+        List<HeadLine> headLineList = headLineDao.queryHeadLine(new HeadLine());
+        assertEquals(4, headLineList.size());
+    }
+}
