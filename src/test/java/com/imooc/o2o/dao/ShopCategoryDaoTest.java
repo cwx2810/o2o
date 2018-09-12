5acd2309788ba75d1d828c4af132156238a708f9
cwx2810 <cwx2810@163.com>
2018-09-12 15:54:15 +0800

    让店铺类别查询支持查询一级分类

--- src/test/java/com/imooc/o2o/dao/ShopCategoryDaoTest.java
+++ src/test/java/com/imooc/o2o/dao/ShopCategoryDaoTest.java
@@ -22,19 +22,7 @@ public class ShopCategoryDaoTest extends BaseTest {
     public void testQueryShopCategory() {
         // 测试查询分类总数
         List<ShopCategory> shopCategoryList =
-                shopCategoryDao.queryShopCategory(new ShopCategory());
-        assertEquals(2, shopCategoryList.size());
-
-        // 测试查询子类类别数
-        ShopCategory childCategory = new ShopCategory();
-        ShopCategory parentCategory = new ShopCategory();
-        // 设置父类是咖啡奶茶
-        parentCategory.setShopCategoryId(1L);
-        // 设置子类的父亲是父类
-        childCategory.setParent(parentCategory);
-        // 单独查询子类中的类别
-        shopCategoryList = shopCategoryDao.queryShopCategory(childCategory);
-        assertEquals(1, shopCategoryList.size());
-        System.out.println(shopCategoryList.get(0).getShopCategoryName());
+                shopCategoryDao.queryShopCategory(null);
+        System.out.println(shopCategoryList.size());
     }
 }
