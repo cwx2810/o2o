a5af004da1a7f1b76c50ac674eef0f4f925a9fdc
cwx2810 <cwx2810@163.com>
2018-09-12 16:39:55 +0800

    前后端联调2'

---
+++ src/main/java/com/imooc/o2o/web/frontend/FrontendController.java
@@ -0,0 +1,19 @@
+package com.imooc.o2o.web.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author: LieutenantChen
+ * @create: 2018-09-12 15:35
+ **/
+@Controller
+@RequestMapping(value = "/frontend", method = RequestMethod.GET)
+public class FrontendController {
+
+    @RequestMapping(value = "/index")
+    private String index() {
+        return "frontend/index";
+    }
+}
